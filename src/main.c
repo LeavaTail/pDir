fc5c82b7df566875dc93ee184cba0c89cc8f4350
LeavaTail <starbow.duster@gmail.com>
2019-08-15 14:23:11 +0900

    [Add] Create command-line argument parsing templete.
    -a, --all: print all FILEs (included . and ..)

--- src/main.c
+++ src/main.c
@@ -4,9 +4,46 @@
  * Print Directory entry (like 'ls').
  */
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include <config.h>
+#include <getopt.h>
 #include "gettext.h"
 #define _(String) gettext (String)
+
+#define CMDLINE_FAILURE	1
+
+static bool print_all;
+
+static struct option const longopts[] =
+{
+	{"all", no_argument, NULL, 'a'},
+	{0,0,0,0}
+};
+
+void usage(int status)
+{
+	exit(status);
+}
+
+static int decode_cmdline(int argc, char **argv)
+{
+	int longindex = 0;
+	int opt = 0;
+
+	while((opt = getopt_long(argc, argv,
+		"a",
+		longopts, &longindex)) != -1) {
+		switch(opt) {
+			case 'a':
+				print_all = true;
+				break;
+			default:
+				usage(CMDLINE_FAILURE);
+		}
+	}
+	return optind;
+}
 
 int main(int argc, char *argv[])
 {
@@ -14,6 +51,7 @@ int main(int argc, char *argv[])
 	bindtextdomain (PACKAGE, LOCALEDIR);
 	textdomain (PACKAGE);
 
+	decode_cmdline(argc, argv);
 	printf(gettext("hello, world\n"));
 	return 0;
 }
