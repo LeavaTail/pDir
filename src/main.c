07d6b4924051b0d256e5041711e1875b129180ed
LeavaTail <starbow.duster@gmail.com>
2019-08-16 13:51:01 +0900

    [Add] Add usage message.
    --help: print usage message to stdout

--- src/main.c
+++ src/main.c
@@ -8,22 +8,39 @@
 #include <stdbool.h>
 #include <config.h>
 #include <getopt.h>
+#include <limits.h>
 #include "gettext.h"
 #include "list.h"
 #define _(String) gettext (String)
+#define PROGRAM_NAME	"pdir"
 
-#define CMDLINE_FAILURE	1
+#define ALLOCATION_FAILURE	1
+#define CMDLINE_FAILURE	2
+
+#define GETOPT_HELP_CHAR	(CHAR_MIN - 2)
 
 static bool print_all;
 
 static struct option const longopts[] =
 {
 	{"all", no_argument, NULL, 'a'},
+	{"help",no_argument, NULL, GETOPT_HELP_CHAR},
 	{0,0,0,0}
 };
 
 void usage(int status)
 {
+	FILE *out;
+	switch(status) {
+		case CMDLINE_FAILURE:
+			out = stderr;
+			break;
+		default:
+			out = stdout;
+	}
+	fprintf(out, _("Usage: %s [OPTION]... [FILE]...\n"),
+			PROGRAM_NAME);
+
 	exit(status);
 }
 
@@ -39,6 +56,9 @@ static int decode_cmdline(int argc, char **argv)
 		switch(opt) {
 			case 'a':
 				print_all = true;
+				break;
+			case GETOPT_HELP_CHAR:
+				usage(EXIT_SUCCESS);
 				break;
 			default:
 				usage(CMDLINE_FAILURE);
@@ -71,6 +91,5 @@ int main(int argc, char *argv[])
 
 	}
 
-	printf(gettext("hello, world\n"));
 	return 0;
 }
