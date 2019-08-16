4b1ae0041133fe9ab9d30757e543333baf59909a
LeavaTail <starbow.duster@gmail.com>
2019-08-16 19:19:26 +0900

    [Add] Decode argument file path & print file name.

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@
 #include <config.h>
 #include <getopt.h>
 #include <limits.h>
+#include <sys/stat.h>
 #include "gettext.h"
 #include "list.h"
 #define _(String) gettext (String)
@@ -16,17 +17,9 @@
 
 #define ALLOCATION_FAILURE	1
 #define CMDLINE_FAILURE	2
+#define ALLOCATE_COUNT	100
 
 #define GETOPT_HELP_CHAR	(CHAR_MIN - 2)
-
-static bool print_all;
-
-static struct option const longopts[] =
-{
-	{"all", no_argument, NULL, 'a'},
-	{"help",no_argument, NULL, GETOPT_HELP_CHAR},
-	{0,0,0,0}
-};
 
 void usage(int status)
 {
@@ -43,6 +36,15 @@ void usage(int status)
 
 	exit(status);
 }
+
+static bool print_all;
+
+static struct option const longopts[] =
+{
+	{"all", no_argument, NULL, 'a'},
+	{"help",no_argument, NULL, GETOPT_HELP_CHAR},
+	{0,0,0,0}
+};
 
 static int decode_cmdline(int argc, char **argv)
 {
@@ -73,8 +75,125 @@ static int decode_cmdline(int argc, char **argv)
 	return optind;
 }
 
+/**
+ * struct fileinfo - File information.
+ * @name:   File name
+ * @status: File status
+ */
+struct fileinfo {
+	char *name;
+	struct stat status;
+};
+
+//! File information slots
+static struct fileinfo *files;
+//! allocate `fileinfo` count in slots
+static size_t alloc_count;
+//! index of first unused `fileinfo` count in slots
+static size_t unused_index;
+
+
+/**
+ * init_slots - Initialize File information slots
+ *
+ * File information slots initialize.(allocation count, index,...)
+ */
+static void init_slots()
+{
+	alloc_count = 0;
+	unused_index = 0;
+}
+
+/**
+ * addfiles_slots - Add a File information to slots
+ * @name:   File name
+ *
+ * Return: 0 - success
+ *         otherwise - error(show ERROR STATUS CODE)
+ */
+static int addfiles_slots(char const *name)
+{
+	int err = 0;
+	struct fileinfo *finfo;
+
+	if(alloc_count <= unused_index) {
+		files = realloc(files, alloc_count+=ALLOCATE_COUNT);
+		if(!files) {
+			perror("expanded file slots arae");
+			free(files);
+			exit(ALLOCATION_FAILURE);
+		}
+		alloc_count += ALLOCATE_COUNT;
+	}
+	finfo = &files[unused_index++];
+	memset(finfo, '\0', sizeof *finfo);
+
+	finfo->name = malloc((strlen(name)+1) * sizeof *name);
+	if(!finfo->name) {
+		perror("allocate file name");
+		err = ALLOCATION_FAILURE;
+		goto errout;
+	}
+	strncpy(finfo->name, name, strlen(name)+1);
+
+	err = lstat(name, &finfo->status);
+	if(err) {
+		perror("cannot access");
+		goto errout;
+	}
+
+errout:
+	return err;
+}
+
+/**
+ * __printfiles_slots - Print the file name
+ * @out:    Output streams
+ * @f:      File information.
+ *
+ * Return:  Number of items write
+ */
+static size_t __printfiles_slots(FILE *out, const struct fileinfo f)
+{
+	size_t len = 0;
+	const char* name = f.name;
+
+	if(out != NULL)
+		len = fwrite(name, sizeof(char), strlen(name), out);
+	return len;
+}
+
+/**
+ * printfiles_slots - List all the files in slots
+ */
+static void printfiles_slots()
+{
+	int i;
+
+	for(i = 0; i < unused_index; i++) {
+		__printfiles_slots(stdout, files[i]);
+		putchar('\n');
+	}
+}
+
+/**
+ * clean_slots - clean up file information slots
+ *
+ * WARN: Be sure clean up list when use slots.
+ */
+static void clean_slots()
+{
+	int i;
+	for(i = unused_index; i >= 0;  i--, unused_index--) {
+		free(files[i].name);
+	}
+
+	free(files);
+}
+
 int main(int argc, char *argv[])
 {
+	int i;
 	int optind;
 	int n_files;
 
@@ -85,11 +204,22 @@ int main(int argc, char *argv[])
 	optind = decode_cmdline(argc, argv);
 	n_files = argc - optind;
 
-	if(n_files <= 0) {
-
-	} else {
-
+	init_slots();
+	alloc_count = ALLOCATE_COUNT;
+	files = malloc (alloc_count * (sizeof *files));
+	if(!files) {
+		perror("Initialize File slots");
+		exit(ALLOCATION_FAILURE);
 	}
 
+	if(n_files <= 0) {
+		addfiles_slots(".");
+	} else {
+		for(i = optind; i < argc; i++)
+			addfiles_slots(argv[i]);
+	}
+
+	printfiles_slots();
+	clean_slots();
 	return 0;
 }
